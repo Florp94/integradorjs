6106a4cd2d6cf66eaf34cd3b5c8551f6b28796d8
Florencia Pagella <flo.pagella94@hotmail.com>
2023-07-04 20:37:20 -0300

    primer render

--- main.js
+++ main.js
@@ -1,9 +1,30 @@
 const productContainer = document.querySelector(".parent");
 
+const createProductTemplate = (product) => {
+    const {id, name, precio, cardImg} = product;
 
 
-const renderProducts = () => {
+    return `
+        <div class="card_parent">
+            <img src=${cardImg} alt=${name}>
+            <div class="fav_filter">
+                <i class="fa-regular fa-heart"></i>
+                <p>Hilo</p>
+            </div>
+            <h4>${name}</h4>
+            <div class="color">
+                <button>B</button>
+                <button>L</button>
+                <button>R</button>
+            </div>
+            <h2> $<span>${precio}</span> </h2>
+            <button class="a_compra" data-id="${id}" data-name="${name}" data-precio="${precio}" data-img="${cardImg}" href="#"> <i class="fa-solid fa-cart-plus"></i> Agregar al carrito</button>
+        </div>
+    `
+}
 
+const renderProducts = (productsList) => {
+    productContainer.innerHTML = productsList.map(createProductTemplate).join("");
 }
 
 
@@ -13,7 +34,7 @@ const renderProducts = () => {
 
 
 const init = () => {
-    renderProducts();
+    renderProducts(productData);
 };
 
 init();
